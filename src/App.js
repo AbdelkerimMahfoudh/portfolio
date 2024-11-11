ad3a68b6be66b845efe05b8cf8bcebe7903a1c56
AbdelkerimMahfoudh <abdelkerim.mahfoudh@gmail.com>
2024-11-11 06:15:45 +0500

    Updating the navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
 import './App.css';
-
+import './components/navbar'
 function App() {
   return (
     <div className="App">
-      
+        <Navbar />
     </div>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,8 @@
 import { useState } from "react";
 
 const NavBar = () => {
-    const [active, setActive] = useState('Home');
-    return (
-        <nav>
-            <ul>
-                <li className={active === 'Home' ? 'active' : ''}>Home</li>
-                <li className={active === 'About' ? 'active' : ''}>About</li>
-                <li className={active === 'Portfolios' ? 'active' : ''}>Portfolios</li>
-                <li className={active === 'Contact' ? 'active' : ''}>Contact</li>
-            </ul>
-        </nav>
-    );
-};
+        
+    return
+}
 
 export default NavBar;
